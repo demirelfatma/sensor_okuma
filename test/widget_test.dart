b00e3cb91697474171036dbcd4d13ec70718f750
FATMA DEMİREL <demirelfatma034@gmail.com>
2022-11-11 10:25:29 +0300

    main sayfası düzenlendi

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:sensor_okuma/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const VibratingApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
